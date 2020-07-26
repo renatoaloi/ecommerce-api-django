8514318f54f9c6bc838e91e41df552ce062b62bd
Renato Aloi <renato.aloi@bevipag.com.br>
2020-07-26 17:24:49 -0300

    another try on CI

--- infra/ci.sh
+++ infra/ci.sh
@@ -2,12 +2,12 @@
 
 export TERM=xterm-256color
 
-apt-get update && apt-get install -y jq
+sudo apt-get update && apt-get install -y jq
 pip3 install awscli
 
 if [[ ! `jq --version ` ]]; then
     echo "jq not found"
 fi
-apt install -y awscli
+sudo apt install -y awscli
 
 bash push.sh "$1"
