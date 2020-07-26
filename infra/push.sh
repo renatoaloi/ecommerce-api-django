4b3433d2125157163dddff8706234e5712ccab7a
Renato Aloi <renato.aloi@bevipag.com.br>
2020-07-26 17:34:27 -0300

    path adjustment

--- infra/push.sh
+++ infra/push.sh
@@ -24,7 +24,7 @@ success () {
 
 
 load_config(){
-  if [[ ! -e "config.json" ]]; then
+  if [[ ! -e "infra/config.json" ]]; then
     error "Missing ecs.config.json"
   fi
   CONFIG=`cat config.json | jq -r .`
